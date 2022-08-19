68046d26b147a1f35458fe913f3dcb5ea47a93b3
LucasApalosqui <lucas.apalosqui@gmail.com>
2022-08-19 10:49:33 -0300

    endpoint with body json integration

--- Program.cs
+++ Program.cs
@@ -1,17 +1,42 @@
-//Inicia e constroi a aplicação para rodar em servidor local
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace api6;
+using System.Globalization;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //Inicia e constroi a aplicação para rodar em servidor local
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
 
-//endpoint simples para pegar informações
-app.MapGet("/", () => "Hello World!");
+        //endpoint simples para pegar informações
+        app.MapGet("/", () => "Hello World!");
 
-//endpoint de postagem com informação json via lambda
-app.MapPost("/user", () => new {name = "Juanito nieves de la vaganza", Age = 67, Hobbie = "Exportar hipopotamos"});
+        //endpoint de postagem com informação json via lambda
+        app.MapPost("/user", () => new { name = "Juanito nieves de la vaganza", Age = 67, Hobbie = "Exportar hipopotamos" });
 
-//endpoint com alteração de headers e get de informações imbutido
-app.MapGet("/user", (HttpResponse response) => {
-    response.Headers.Add("Teste", "Paozinho");
-    return new {name = "Juanito nieves de la vaganza", Age = 67, Hobbie = "Exportar hipopotamos"};
-});
+        //endpoint com alteração de headers e get de informações imbutido
+        app.MapGet("/user", (HttpResponse response) =>
+        {
+            response.Headers.Add("Teste", "Paozinho");
+            return new { name = "Juanito nieves de la vaganza", Age = 67, Hobbie = "Exportar hipopotamos" };
+        });
 
-app.Run();
+        //endpoint que adiciona um novo produto via json atráves do body
+        app.MapPost("/Product", (Product product) =>{
+            return "name: " + product.Name + " - " + product.Price.ToString("F2", CultureInfo.InvariantCulture) + " - " + product.Status;
+        });
+
+        
+
+        app.Run();
+    }
+}
+
+//Classe auxiliar para adicionar um novo produto
+public class Product {
+    public string? Name { get; set; }
+    public double Price { get; set; }
+    public string? Status { get; set; }
+}
+
+
