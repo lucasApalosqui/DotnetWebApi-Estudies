ac676d71e44a456b076f29d542ee733b5d568f81
LucasApalosqui <lucas.apalosqui@gmail.com>
2022-08-19 14:18:13 -0300

    refactoring endpoints and add gen status code

--- Program.cs
+++ Program.cs
@@ -13,13 +13,19 @@ public class Program
         //endpoint que adiciona um novo produto via json atráves do body
         app.MapPost("/products", (Product product) =>{
            ProductRepository.addProduct(product);
+           return Results.Created($"/products/{product.Name}", product.Name);
         });
 
         //endpoint que recebe dados via code route
         //api.com.br/users/{code}
         app.MapGet("/products/{name}", ([FromRoute]string name) =>{
             var product = ProductRepository.GetBy(name);
-            return product;
+            if(product != null){
+                return Results.Ok(product);
+            }else{
+                return Results.NotFound("num achei naum");
+            }
+            
         });
 
         //endpoint que altera um produto
@@ -27,17 +33,15 @@ public class Program
            var productSave = ProductRepository.GetBy(product.Name);
            productSave.Price = product.Price;
            productSave.Status = product.Status;
+           return Results.Ok();
         });
 
         //endpoint que deleta um produto pelo seu nome
         app.MapDelete("/products/{name}", ([FromRoute]string name) =>{
           var productDelete = ProductRepository.GetBy(name);
           ProductRepository.Remove(productDelete);
+          return Results.Ok();
         });
-
-
-
-        
 
         app.Run();
     }
@@ -64,8 +68,8 @@ public static class ProductRepository{
     }
 
     //função que pega um produto especifico de acordo com o parametro indicado
-    public static Product GetBy(string code){
-        return products.First(p => p.Name == code);
+    public static Product GetBy(string name){
+        return products.FirstOrDefault(p => p.Name == name);
     }
 
     //função que deleta um produto da lista de produtos
