0783bda0765c637a7a2527c7c8995948a9aaf25b
LucasApalosqui <lucas.apalosqui@gmail.com>
2022-08-19 13:15:36 -0300

    add endpoints can get, post, put and delete products

--- Program.cs
+++ Program.cs
@@ -25,8 +25,9 @@ public class Program
 
         //endpoint que adiciona um novo produto via json atráves do body
         app.MapPost("/Product", (Product product) =>{
-            return "name: " + product.Name + " - " + product.Price.ToString("F2", CultureInfo.InvariantCulture) + " - " + product.Status;
+           ProductRepository.addProduct(product);
         });
+        
 
         //endpoint que recebe dados via query
         //api.com.br/users?datastart={date}&dataend={date}
@@ -37,12 +38,26 @@ public class Program
         //endpoint que recebe dados via code route
         //api.com.br/users/{code}
         app.MapGet("/getProduct/{code}", ([FromRoute]string code) =>{
-            return code;
+            var product = ProductRepository.GetBy(code);
+            return product;
         });
 
         //endpoint que recebe dados via header da aplicação
         app.MapGet("/getProducts", (HttpRequest request) => {
             return request.Headers["product-id"].ToString();
+        });
+
+        //endpoint que altera um produto
+         app.MapPut("/ProductUpdate", (Product product) =>{
+           var productSave = ProductRepository.GetBy(product.Name);
+           productSave.Price = product.Price;
+           productSave.Status = product.Status;
+        });
+
+        //endpoint que deleta um produto pelo seu nome
+        app.MapDelete("/ProductDelete/{name}", ([FromRoute]string name) =>{
+          var productDelete = ProductRepository.GetBy(name);
+          ProductRepository.Remove(productDelete);
         });
 
 
@@ -58,6 +73,30 @@ public class Product {
     public string? Name { get; set; }
     public double Price { get; set; }
     public string? Status { get; set; }
+}
+
+//classe auxiliar com funções de alteração no endpoint
+public static class ProductRepository{
+    public static List<Product> products = new List<Product>();
+
+    //função que adiciona um produto na lista de produtos
+    public static void addProduct(Product product){
+        if(products == null){
+        products = new List<Product>();
+        
+        }
+        products.Add(product);
+    }
+
+    //função que pega um produto especifico de acordo com o parametro indicado
+    public static Product GetBy(string code){
+        return products.First(p => p.Name == code);
+    }
+
+    //função que deleta um produto da lista de produtos
+    public static void Remove(Product product){
+        products.Remove(product);
+    }
 }
 
 
